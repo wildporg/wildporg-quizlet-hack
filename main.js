1653ea4d571d4ae6c97f557269a2aa5a28003f01
wildporg <help@wildporg.com>
2023-07-02 13:31:01 +0100

    Bugs Fixed, code is cleaner.

--- main.js
+++ main.js
@@ -1,54 +1,53 @@
 // ==UserScript==
 // @name         wildporg Colors Exploit
-// @namespace    https://github.com/wildporg/wildporg-quizlet-hack/blob/main/main.js
-// @version      1.1.1
+// @namespace    https://github.com/wildporg/wildporg-quizlet-hack/
+// @version      1.1.3
 // @description  Color matching items in quizlet match.
 // @author       wildporg (https://github.com/wildporg/)
+// @downloadURL  https://github.com/wildporg/wildporg-quizlet-hack/raw/site-intergration/main.user.js
+// @updateURL    https://github.com/wildporg/wildporg-quizlet-hack/raw/main/main.js
 // @match        https://quizlet.com/*/*
 // @icon         https://github.com/favicon.ico
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
 
-var answers;
-var i = 0;
+var tiles;
 var x;
-var colors = ["","#FF0000","#FFFF00","#00FF00","#00FFFF","#0000FF","#FF6600"];
-var isEnabled;
-var wasEnabled;
-var inputNode;
+var colors = ['', '#FF0000','#FFFF00','#00FF00','#00FFFF','#0000FF','#FF6600'];
+var state;
+var wasEnabled = false;
 
 (function() {
-    isEnabled = window.localStorage.getItem("hackEnabled");
-    if (isEnabled == "true") {
-        isEnabled = " checked";
-    } else if (isEnabled == "false") {
-        isEnabled = "";
+    if (GM_getValue('wp-quizlet-hack-enabled')) {
+        state = 'checked';
     } else {
-        isEnabled = " checked";
-        window.localStorage.setItem("hackEnabled", "true");
+        state = '';
+        GM_setValue('wp-quizlet-hack-enabled', false);
     };
-    inputNode = document.createElement('div');
-    inputNode.style.display = 'flex';
-    inputNode.innerHTML = '<input type="checkbox"'+isEnabled+'><p>Enabled</p>';
     setInterval( function() {
+        if (document.querySelectorAll('.MatchModeQuestionScatterTile').length > 0 && document.querySelector('.ModeControls-main').children.length == 3) {
+            document.querySelector('.ModeControls-main').innerHTML += '<div style="display: flex;"><input type="checkbox" id="wp-hack-checkbox" '+state+'><label for="wp-hack-checkbox">Enabled</label></div>';
+            tiles = document.querySelectorAll('.MatchModeQuestionScatterTile');
+        };
         if (document.getElementsByClassName("MatchModeQuestionScatterTile").length > 0) {
-            if (i == 0) {
-                document.getElementsByClassName("ModeControls-main")[0].append(inputNode);
-                answers = document.getElementsByClassName("MatchModeQuestionScatterTile");
-                i = 1;
-            };
-            if (document.getElementsByClassName("ModeControls-main")[0].lastElementChild.firstElementChild.checked && !wasEnabled) {
-                wasEnabled = true;
-                window.localStorage.setItem("hackEnabled", "true");
-                for(x = 0; x < answers.length; x++) {
-                    answers[x].style.background = colors[Math.round((x+1)/2)];
+            if (document.querySelector('#wp-hack-checkbox').checked) {
+                console.log('no problemo here')
+                if (!wasEnabled) {
+                    GM_setValue('hackEnabled', true);
+                    console.log('was not enabled. ok')
+                    for(x = 0; x < tiles.length; x++) {
+                        console.log(x);
+                        tiles[x].style.background = colors[Math.round((x+1)/2)];
+                    };
+                    wasEnabled = true;
                 };
             } else {
-                wasEnabled = false;
-                window.localStorage.setItem("hackEnabled", "false");
-                for(x = 0; x < answers.length; x++) {
-                    answers[x].style.background = "";
+                GM_setValue('hackEnabled', false);
+                for(x = 0; x < tiles.length; x++) {
+                    tiles[x].style.background = '';
                 };
+                wasEnabled = false;
             };
         };
     }, 100);
